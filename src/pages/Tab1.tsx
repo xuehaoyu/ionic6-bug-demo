38c1fb9c9a843a039715087487ec1fc736d6237a
xuehaoyu <1169732429@qq.com>
2022-12-28 10:34:10 +0800

    init

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonModal, IonList, IonItem, IonLabel } from '@ionic/react';
+import { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+const Dom: React.FC = (props: any) => {
+  return <IonModal isOpen>
+    <IonContent>
+      <IonList>
+        <IonItem onClick={() => {
+          console.log('11111')
+        }}>
+          <IonLabel>Pokémon Yellow</IonLabel>
+        </IonItem>
+      </IonList>
+    </IonContent>
+  </IonModal>
+}
+
 const Tab1: React.FC = () => {
+  useEffect(() => {
+    const createDiv = document.createElement('div')
+    const parentDom = document.body
+    parentDom.appendChild(createDiv)
+    const root = createRoot(createDiv);
+    root.render(<Dom></Dom>)
+  }, [])
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +41,7 @@ const Tab1: React.FC = () => {
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
+      {/* <Dom></Dom> */}
     </IonPage>
   );
 };
